ad6951096bd03a349e6ee91fb8f4f018f9761f5f
Ewa Dwornikowska <ewa.dwornikowska@gmail.com>
2014-08-09 16:49:26 +0200

    removed box in plot4 legend

--- plot4.R
+++ plot4.R
@@ -20,7 +20,7 @@ createPlot4 <- function() {
   lines(data$Time, data$Sub_metering_1)
   lines(data$Time, data$Sub_metering_2, col='red')
   lines(data$Time, data$Sub_metering_3, col='blue')
-  legend('topright', border='white', lty=c(1,1,1), col=c('black', 'red', 'blue'), legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'))
+  legend('topright', bty="n", lty=c(1,1,1), col=c('black', 'red', 'blue'), legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'))
 
   ## Gloabl Reactive Power plot
   plot(data$Time, data$Global_reactive_power, type="l", ylab='Global_reactive_power', xlab='datetime')
